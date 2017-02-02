0542f5e73cfa296e04d5f92dce7450ed713b9b4f
Akshit Sahani <akshit_sahani@hotmail.com>
2017-02-02 15:32:13 -0500

    completed with stretch. Rolled back deleted table

--- db/migrate/20170202200432_delete_widgets.rb
+++ db/migrate/20170202200432_delete_widgets.rb
@@ -1,5 +1,22 @@
 class DeleteWidgets < ActiveRecord::Migration[5.0]
   def change
-    drop_table :widgets
+    reversible do |d|
+      d.up do
+        drop_table :widgets
+      end
+      d.down do
+        create_table :widgets do |t|
+        t.string :first_name
+        t.string :last_name
+        t.string :dietary_restrictions
+        t.integer :salary
+        t.integer :number_of_kids
+        t.string :vulnerabilities
+        t.string :illnesses
+        t.string :medication
+        t.string :voting_preferences
+      end
+    end
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202200432) do
+ActiveRecord::Schema.define(version: 20170202200126) do
 
   create_table "locations", force: :cascade do |t|
     t.string  "name"
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 20170202200432) do
     t.string  "cost"
     t.string  "decimal"
     t.decimal "quantity"
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.string  "first_name"
+    t.string  "last_name"
+    t.string  "dietary_restrictions"
+    t.integer "salary"
+    t.integer "number_of_kids"
+    t.string  "vulnerabilities"
+    t.string  "illnesses"
+    t.string  "medication"
+    t.string  "voting_preferences"
   end
 
 end
